70dd18df8d1c1c6f6ad38b3c2cacb5e5386d2582
WangLuofan <wangluofan@gmail.com>
2017-10-16 22:46:08 +0800

    Init MyMovie Project

---
+++ MyMovie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyMovie
+//
+//  Created by 王落凡 on 2017/10/16.
+//  Copyright © 2017年 王落凡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyMovie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyMovie
+//
+//  Created by 王落凡 on 2017/10/16.
+//  Copyright © 2017年 王落凡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
